4dff5266509aedb51eb9771bee0204635586448a
Daniel Jackoway <commits@danieljackoway.com>
2022-12-10 23:36:22 -0800

    day one done

--- day01/one.ts
+++ day01/one.ts
@@ -1,6 +1,5 @@
 import { readFile } from "../utils";
-
-async function partOne() {
+async function elfCalories() {
   let elves = [0];
   const lines = await readFile();
   console.log(lines);
@@ -11,7 +10,16 @@ async function partOne() {
       elves[elves.length - 1] += parseInt(line);
     }
   }
-  //console.log(elves);
+  return elves;
+}
+async function partOne() {
+  const elves = await elfCalories();
   return Math.max(...elves);
 }
 partOne().then(console.log);
+async function partTwo() {
+  const elves = await elfCalories();
+  elves.sort((a, b) => b - a);
+  return elves[0] + elves[1] + elves[2];
+}
+partTwo().then(console.log);
